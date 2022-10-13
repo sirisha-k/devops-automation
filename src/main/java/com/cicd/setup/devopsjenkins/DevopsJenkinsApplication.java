83bfe7ebf05e985bda9655a87f821313ff727907
sirisha_k <s.koya@samsung.com>
2022-10-13 15:29:53 -0400

    added get mapping

--- src/main/java/com/cicd/setup/devopsjenkins/DevopsJenkinsApplication.java
+++ src/main/java/com/cicd/setup/devopsjenkins/DevopsJenkinsApplication.java
@@ -2,11 +2,17 @@ package com.cicd.setup.devopsjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class DevopsJenkinsApplication {
 
+	@GetMapping(/welcome)
+	public String message(){
+	   return "welcome to siri program";
+   }
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsJenkinsApplication.class, args);
 	}
