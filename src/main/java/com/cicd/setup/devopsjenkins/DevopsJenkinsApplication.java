2fd677a065058861dc45a01db7b401409a072171
sirisha_k <s.koya@samsung.com>
2022-10-12 20:04:57 -0400

    devops

---
+++ src/main/java/com/cicd/setup/devopsjenkins/DevopsJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.cicd.setup.devopsjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DevopsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/setup/devopsjenkins/DevopsJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.setup.devopsjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
